c32a7786bc9ab4dfc5942309fa216af64e2275ca
Milenagms <milenagomes022@gmail.com>
2023-02-24 20:13:32 -0300

    Occuapancy Area: Listagem de todas as areas daquele usuario

---
+++ src/modules/freelancerInformation/useCases/listOccupancyArea/ListAreasUseCase.ts
@@ -0,0 +1,21 @@
+import { inject, injectable } from 'tsyringe'
+import { OccupancyArea } from '@/modules/freelancerInformation/infra/typeorm/entities/OccupancyArea'
+
+import { IOccupancyAreaRepository } from '@/modules/freelancerInformation/repositories/IOccupancyAreaRepository'
+
+@injectable()
+class ListAreasUseCase {
+
+    constructor(
+		@inject('OccupancyAreaRepository')
+        private OccupancyAreaRepository: IOccupancyAreaRepository,
+    ) {}
+
+    async execute(user_id: string): Promise<OccupancyArea[]> {
+        return await this.OccupancyAreaRepository.findByUserID(user_id)
+        
+    }
+
+}
+
+export { ListAreasUseCase }
--- src/shared/infra/http/routes/occupancyArea.routes.ts
+++ src/shared/infra/http/routes/occupancyArea.routes.ts
@@ -1,13 +1,21 @@
 import { Router } from 'express'
 import { CreateAreaController } from '@/modules/freelancerInformation/useCases/createOccupancyArea/CreateAreaController'
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated'
+import { ListAreasController } from '@/modules/freelancerInformation/useCases/listOccupancyArea/ListAreasController'
 const occupancyRoutes = Router()
 
 const createAreaController = new CreateAreaController()
+const listAreasController = new ListAreasController()
 occupancyRoutes.post(
 	'/',
 	ensureAuthenticated,
 	createAreaController.handle,
+)
+
+occupancyRoutes.get(
+	'/',
+	ensureAuthenticated,
+	listAreasController.handle
 )
 
 export { occupancyRoutes }
